4e69c4e94088ecaa7e98c8462704933dbb70387d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-09-21 22:19:37 +0900

    :fire:

--- src/components/layout/PublicLayout/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC, ReactNode } from "react";
-
-type LayoutProps = {
-  children: ReactNode;
-};
-
-export const PublicLayout: FC<LayoutProps> = ({ children }) => {
-  return <main>{children}</main>;
-};
