9ae180cd8df843f1a338b5007e4664542cf092b6
miyasan_0301 <miyasan.19990301@gmail.com>
2022-09-24 03:03:56 +0900

    :lipstick:

--- src/components/shared/Divider/index.tsx
+++ src/components/shared/Divider/index.tsx
@@ -3,7 +3,7 @@ import { Divider as MantineDivider } from "@mantine/core";
 export const Divider = () => {
   return (
     <MantineDivider
-      my="xs"
+      mt="xs"
       sx={(theme) => ({
         border: `0.5px solid ${theme.colorScheme === "light" ? theme.colors.gray[3] : theme.colors.dark[5]}`,
       })}
