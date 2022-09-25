4251ddb98e6fda2380149498b9f6c10aced2cac1
miyasan_0301 <miyasan.19990301@gmail.com>
2022-09-26 03:49:48 +0900

    ✨ オプション開閉機能作成

--- src/components/feature/form/JsonFormField.tsx
+++ src/components/feature/form/JsonFormField.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon, Button, Group, Select, Stack, TextInput, Tooltip } from "@mantine/core";
-import { IconChevronUp, IconX } from "@tabler/icons";
+import { IconChevronDown, IconChevronUp, IconX } from "@tabler/icons";
 import type { FC } from "react";
 import { Fragment, useCallback } from "react";
 import type { Control, UseFormRegister } from "react-hook-form";
@@ -11,6 +11,7 @@ import { NumberFormField } from "~/components/feature/form/NumberFormField";
 import { ObjectFormField } from "~/components/feature/form/ObjectFormField";
 import { StringFormField } from "~/components/feature/form/StringFormField";
 import { FormTypeWatch } from "~/components/feature/form/watcher/FormTypeWatch";
+import { OptionVisibleWatch } from "~/components/feature/form/watcher/OptionVisibleWatch";
 import { Divider } from "~/components/shared/Divider";
 import { appendValue } from "~/constants/form/appendValue";
 import { objectValueTypeOption } from "~/constants/form/selectOption";
@@ -58,115 +59,124 @@ export const JsonFormField: FC<Props> = ({ control, register, border = true }) =
     >
       {fields.map((item, index) => {
         return (
-          <Fragment key={item.id}>
-            <Stack spacing="xs">
-              <Group spacing="xs" align="end">
-                <TextInput
-                  required
-                  size="xs"
-                  label="key"
-                  {...register(`json.${index}.keyName`, {
-                    required: {
-                      value: true,
-                      message: "key is required",
-                    },
-                  })}
-                  sx={{
-                    flex: 2,
-                  }}
-                />
+          <OptionVisibleWatch key={item.id}>
+            {(isVisible, onToggle) => (
+              <>
+                <Stack spacing="xs">
+                  <Group spacing="xs" align="end">
+                    <TextInput
+                      required
+                      size="xs"
+                      label="key"
+                      {...register(`json.${index}.keyName`, {
+                        required: {
+                          value: true,
+                          message: "key is required",
+                        },
+                      })}
+                      sx={{
+                        flex: 2,
+                      }}
+                    />
 
-                <Controller
-                  control={control}
-                  name={`json.${index}`}
-                  render={({ field: { onChange, value } }) => {
-                    const onChangeValue = (changeValue: ValueType) => {
-                      onChange({
-                        ...value,
-                        keyName: value.keyName,
-                        valueType: changeValue,
-                        options: appendValue[changeValue],
-                      });
-                    };
-                    return (
-                      <Select
-                        size="xs"
-                        label="value"
-                        value={value.valueType}
-                        onChange={onChangeValue}
-                        data={objectValueTypeOption}
-                        sx={{
-                          flex: 1,
-                        }}
-                      />
-                    );
-                  }}
-                />
+                    <Controller
+                      control={control}
+                      name={`json.${index}`}
+                      render={({ field: { onChange, value } }) => {
+                        const onChangeValue = (changeValue: ValueType) => {
+                          onChange({
+                            ...value,
+                            keyName: value.keyName,
+                            valueType: changeValue,
+                            options: appendValue[changeValue],
+                          });
+                        };
+                        return (
+                          <Select
+                            size="xs"
+                            label="value"
+                            value={value.valueType}
+                            onChange={onChangeValue}
+                            data={objectValueTypeOption}
+                            sx={{
+                              flex: 1,
+                            }}
+                          />
+                        );
+                      }}
+                    />
 
-                <Tooltip label="option" position="top-start">
-                  <ActionIcon mb={1} component="button">
-                    <IconChevronUp size={16} />
-                    {/* {isVisible ? <IconChevronUp size={16} /> : <IconChevronDown size={16} />} */}
-                  </ActionIcon>
-                </Tooltip>
+                    <Tooltip label="option" position="top-start">
+                      <ActionIcon mb={1} component="button" onClick={onToggle}>
+                        {isVisible ? <IconChevronUp size={16} /> : <IconChevronDown size={16} />}
+                      </ActionIcon>
+                    </Tooltip>
 
-                <Tooltip label="delete" position="top-start">
-                  <ActionIcon mb={1} component="button" onClick={() => onRemove(index)}>
-                    <IconX size={16} color="red" />
-                  </ActionIcon>
-                </Tooltip>
-              </Group>
+                    <Tooltip label="delete" position="top-start">
+                      <ActionIcon mb={1} component="button" onClick={() => onRemove(index)}>
+                        <IconX size={16} color="red" />
+                      </ActionIcon>
+                    </Tooltip>
+                  </Group>
 
-              <FormTypeWatch name={`json.${index}.valueType`} control={control}>
-                {(value) => {
-                  if (value === "string") {
-                    return (
-                      <StringFormField
-                        name={{
-                          stringDummyType: `json.${index}.options.stringDummyType`,
-                          prefix: `json.${index}.options.prefix`,
-                          suffix: `json.${index}.options.suffix`,
-                        }}
-                      />
-                    );
-                  }
+                  {isVisible ? (
+                    <FormTypeWatch name={`json.${index}.valueType`} control={control}>
+                      {(value) => {
+                        if (value === "string") {
+                          return (
+                            <StringFormField
+                              name={{
+                                stringDummyType: `json.${index}.options.stringDummyType`,
+                                prefix: `json.${index}.options.prefix`,
+                                suffix: `json.${index}.options.suffix`,
+                              }}
+                            />
+                          );
+                        }
 
-                  if (value === "number") {
-                    return <NumberFormField name={{ numberDummy: `json.${index}.options.numberDummyType` }} />;
-                  }
+                        if (value === "number") {
+                          return <NumberFormField name={{ numberDummy: `json.${index}.options.numberDummyType` }} />;
+                        }
 
-                  if (value === "boolean") {
-                    return (
-                      <BooleanFormField
-                        name={{
-                          booleanDummy: `json.${index}.options.booleanDummyType`,
-                        }}
-                      />
-                    );
-                  }
+                        if (value === "boolean") {
+                          return (
+                            <BooleanFormField
+                              name={{
+                                booleanDummy: `json.${index}.options.booleanDummyType`,
+                              }}
+                            />
+                          );
+                        }
 
-                  if (value === "object") {
-                    return (
-                      <ObjectFormField register={register} control={control} name={`json.${index}.options.object`} />
-                    );
-                  }
+                        if (value === "object") {
+                          return (
+                            <ObjectFormField
+                              register={register}
+                              control={control}
+                              name={`json.${index}.options.object`}
+                            />
+                          );
+                        }
 
-                  if (value === "array") {
-                    return (
-                      <ArrayFormField
-                        name={{
-                          item: `json.${index}.options.item`,
-                          length: `json.${index}.options.length`,
-                        }}
-                      />
-                    );
-                  }
-                }}
-              </FormTypeWatch>
-            </Stack>
+                        if (value === "array") {
+                          return (
+                            <ArrayFormField
+                              name={{
+                                item: `json.${index}.options.item`,
+                                length: `json.${index}.options.length`,
+                              }}
+                            />
+                          );
+                        }
+                      }}
+                    </FormTypeWatch>
+                  ) : null}
+                </Stack>
 
-            <Divider />
-          </Fragment>
+                <Divider />
+              </>
+            )}
+          </OptionVisibleWatch>
         );
       })}
 
