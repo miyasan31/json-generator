8f324b715b7f17156fda3d2a6473fd22cd1047f9
miyasan_0301 <miyasan.19990301@gmail.com>
2022-09-26 05:52:04 +0900

    💄 prism syntax highlight

--- src/components/layout/JsonEditor/OutputLayout.tsx
+++ src/components/layout/JsonEditor/OutputLayout.tsx
@@ -1,6 +1,6 @@
-import { Button, CopyButton, Group, JsonInput, Stack } from "@mantine/core";
+import { Button, CopyButton, Group, Stack } from "@mantine/core";
+import { Prism } from "@mantine/prism";
 import { IconCheck } from "@tabler/icons";
-import React from "react";
 import { useFormContext } from "react-hook-form";
 
 import { JsonGeneratorWatch } from "~/components/feature/form/watcher/JsonGeneratorWatch";
@@ -24,16 +24,34 @@ export const OutputLayout = () => {
             <Button type="button">Save</Button>
           </Group>
 
-          <JsonInput
-            value={json}
-            formatOnBlur
-            autosize={false}
-            styles={{
-              root: { flex: 1 },
-              wrapper: { height: "100%" },
-              input: { height: "100%" },
-            }}
-          />
+          <Prism
+            language="json"
+            noCopy
+            withLineNumbers
+            styles={(theme) => ({
+              root: {
+                height: "100%",
+                borderRadius: theme.radius.sm,
+                backgroundColor: theme.colorScheme === "light" ? "white" : theme.colors.dark[6],
+                border: `1px solid ${theme.colorScheme === "light" ? theme.colors.gray[4] : theme.colors.dark[4]}`,
+                minHeight: 0,
+              },
+              scrollArea: {
+                borderRadius: theme.radius.sm,
+                height: "100%",
+              },
+              code: {
+                margin: "16px 0",
+                padding: 0,
+              },
+              line: {
+                height: "100%",
+                backgroundColor: theme.colorScheme === "light" ? "white" : theme.colors.dark[6],
+              },
+            })}
+          >
+            {json}
+          </Prism>
         </Stack>
       )}
     </JsonGeneratorWatch>
