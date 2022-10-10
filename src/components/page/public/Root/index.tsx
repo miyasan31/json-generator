a2b24adadfd26f4a8ed27d6117a39ef07ca952b6
miyasan_0301 <miyasan.19990301@gmail.com>
2022-10-10 23:43:01 +0900

    ♻️ fetchしないのでSuspense解除

--- src/components/page/public/Root/index.tsx
+++ src/components/page/public/Root/index.tsx
@@ -1,16 +1,13 @@
 import { Head } from "~/components/lib/react-helmet-async/Head";
 import { ErrorBoundary } from "~/components/provider/ErrorBoundary";
-import { Suspense } from "~/components/provider/Suspense";
 
 import { Root } from "./Root.page";
 
 export const RootPage = () => {
   return (
     <ErrorBoundary>
-      <Suspense>
-        <Head title="JSON Generator" description="JSON Generator" />
-        <Root />
-      </Suspense>
+      <Head title="JSON Generator" description="JSON Generator" />
+      <Root />
     </ErrorBoundary>
   );
 };
